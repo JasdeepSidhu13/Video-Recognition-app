679719fc410b7a920ee1809a73afaff86c2a0f2f
JasdeepSidhu13 <jasdeepsidhu13@gmail.com,jasdeepsidhu@ufl.edu>
2018-01-30 18:36:14 -0500

    First comit

---
+++ backend/index.js
@@ -0,0 +1,74 @@
+// Copyright 2017 Google Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const config = require('./local.json')
+
+const storage = require('@google-cloud/storage');
+const storageClient = storage({
+  projectId: config.cloud_project_id,
+  keyfileName: "keyfile.json"
+});
+
+const video = require('@google-cloud/video-intelligence');
+const videoClient = video({
+  projectId: config.cloud_project_id,
+  keyfileName: "keyfile.json"
+});
+
+
+const allowedFileTypes = ["mp4", "mpeg4", "avi", "webm", "mov", "mpg"];
+
+
+exports.analyzeVideo = function analyzeVideo (event, callback) {
+  const file = event.data;
+  const isDelete = file.resourceState === 'not_exists';
+
+  if (isDelete) {
+    console.log(`File ${file.name} deleted.`);
+  } else {
+    const bucketName = file.bucket;
+    console.log('fileinfo: ',file);
+    const fileName = file.name;
+    const fileType = fileName.split('.').pop();
+
+
+    if (allowedFileTypes.indexOf(fileType.toLowerCase()) != -1) {
+      // it's a video!
+      const request = {
+        inputUri: "gs://" + bucketName + '/' + fileName,
+        outputUri: "gs://" + config.video_json_bucket + "/" + fileName.replace(".", "").replace("/", "") + ".json",
+        features: ['LABEL_DETECTION', 'SHOT_CHANGE_DETECTION']
+      }
+
+      videoClient.annotateVideo(request).then(function(responses) {
+        var operation = responses[0];
+        operation.on('complete', function(result, metadata, finalResponse) {
+          console.log('got the annotations: ',result);
+        });
+
+        operation.on('progress', function(metadata,apiResp) {
+          console.log('processing: ', metadata);
+        });
+
+        operation.on('error', function(err) {
+          console.log('error: ', err);
+        });
+      });
+
+    } else {
+      console.log(videoFilename + " is not a video file.")
+    }
+
+  }
+}
---
+++ frontend/resources/js/components/video-card.js
@@ -0,0 +1,48 @@
+// Copyright 2017 Google Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import _ from 'lodash';
+import {VIDEO_DATA} from '../constants';
+
+
+
+
+export default function(video, query, expanded) {
+  const title = video.url_safe_id;
+  const tags = video.annotations.label_annotations.map(label => label.description).join(', ');
+
+  // DEFAULT VALUES
+  let header = `<h3 class="text-body">${title}</h3>`;
+  let selectedTags = tags.length > 90 ? `${tags.substring(0,90)}...` : tags;
+  let isExpanded = '';
+
+  // UPDATE ELEMENTS FOR EXANDED VIDEO CARD
+  if(expanded) {
+    header = `<h3 class="text-title">${title}</h3>`;
+    isExpanded = 'is-expanded';
+    selectedTags = tags.length > 230 ? `${tags.substring(0,230)}...` : tags;
+  }
+
+  // RETURN VIDEO CARD ELEMENT
+  return `
+    <a href="/video/${video.url_safe_id}" class="video-card ${isExpanded}" data-navigo>
+      <div class="video-card-hero">
+        <video class="video-card-video" preload="metadata" id="${video.url_safe_id}" src="${video.link}"></video>
+      </div>
+      ${header}
+      <div class="video-card-graph"></div>
+      <p class="text-caption is-secondary">${selectedTags}</p>
+    </a>
+  `;
+}
